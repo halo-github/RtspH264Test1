870b32da2c706d7fbc6ba4bf23489f21c057e099
halo-github <82198313@qq.com>
2016-05-03 10:32:40 +0800

    Initial Commit

---
+++ RtspH264Test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RtspH264Test1
+//
+//  Created by Fenix on 16/5/3.
+//  Copyright © 2016年 Fenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RtspH264Test1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RtspH264Test1
+//
+//  Created by Fenix on 16/5/3.
+//  Copyright © 2016年 Fenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
